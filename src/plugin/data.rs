232bfcd025ef02b773ce3d12ec31d5246630653e
cameron <cmrnwlsh@gmail.com>
2025-01-17 22:29:01 -0500

    statically loaded toml data

--- src/plugin/data.rs
+++ src/plugin/data.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
 use static_toml::static_toml;
 
+static_toml! {
+    #[derive(Debug)]
+    static ITEMS = include_toml!("data/items.toml");
+}
+
 pub fn plugin(app: &mut App) {
-    static_toml! {
-        #[derive(Debug)]
-        static ITEMS = include_toml!("data/items.toml");
-    }
     app.add_systems(Startup, || {
         info!("{:#?}", ITEMS);
     });

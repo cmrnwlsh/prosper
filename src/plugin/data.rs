36526814e8fa01ff4e66f0a86c7e5b094671d433
cameron <cmrnwlsh@gmail.com>
2025-01-17 22:24:31 -0500

    statically loaded toml data

--- src/plugin/data.rs
+++ src/plugin/data.rs
@@ -1,52 +1,12 @@
-use bevy::{prelude::*, utils::hashbrown::HashMap};
-use bevy_common_assets::toml::TomlAssetPlugin;
-use serde::{Deserialize, Serialize};
+use bevy::prelude::*;
+use static_toml::static_toml;
 
 pub fn plugin(app: &mut App) {
-    app.init_state::<LoadState>()
-        .add_plugins(TomlAssetPlugin::<Data>::new(&["embedded://data.toml"]))
-        .add_systems(Startup, load)
-        .add_systems(Update, poll.run_if(in_state(LoadState::Loading)));
-}
-
-fn load(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(DataHandle(asset_server.load("embedded://data.toml")));
-}
-
-fn poll(
-    mut state: ResMut<NextState<LoadState>>,
-    handle: Res<DataHandle>,
-    asset: Res<Assets<Data>>,
-) {
-    if let Some(asset) = asset.get(&handle.0) {
-        state.set(LoadState::Loaded);
-        info!("\nitems: {:#?}", asset.items.len(),);
+    static_toml! {
+        #[derive(Debug)]
+        static ITEMS = include_toml!("data/items.toml");
     }
-}
-
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
-pub enum LoadState {
-    #[default]
-    Loading,
-    Loaded,
-}
-
-#[derive(Resource)]
-pub struct DataHandle(pub Handle<Data>);
-
-#[derive(Asset, TypePath, Serialize, Deserialize, Debug)]
-pub struct Data {
-    pub items: HashMap<String, Item>,
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum ItemKind {
-    #[serde(alias = "sword")]
-    Sword,
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct Item {
-    pub name: String,
-    pub kind: ItemKind,
+    app.add_systems(Startup, || {
+        info!("{:#?}", ITEMS);
+    });
 }
--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -7,32 +7,28 @@ mod ui;
 
 use bevy::{
     app::{PluginGroup, PluginGroupBuilder, ScheduleRunnerPlugin},
-    asset::AssetPlugin,
     diagnostic::DiagnosticsPlugin,
     hierarchy::HierarchyPlugin,
     state::app::StatesPlugin,
     MinimalPlugins,
 };
-use bevy_embedded_assets::EmbeddedAssetPlugin;
 use std::time::Duration;
 
 pub struct ProsperPlugins;
 impl PluginGroup for ProsperPlugins {
     fn build(self) -> PluginGroupBuilder {
         PluginGroupBuilder::start::<Self>()
-            .add(EmbeddedAssetPlugin::default())
             .add_group(
                 MinimalPlugins.set(ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(
                     1. / 60.,
                 ))),
             )
             .add(StatesPlugin)
-            .add(AssetPlugin::default())
             .add(DiagnosticsPlugin)
             .add(HierarchyPlugin)
             .add(io::plugin)
-            .add(ui::plugin)
             .add(data::plugin)
+            .add(ui::plugin)
             .add(map::plugin)
             .add(sim::plugin)
             .add(log::plugin)

2b20233b00ad7a19a21cd07719f1a6a0723ee440
cameron <cmrnwlsh@gmail.com>
2025-01-31 22:04:22 -0500

    refactor

--- src/plugin/sim/mod.rs
+++ src/plugin/sim/mod.rs
@@ -1,10 +1,11 @@
 pub mod components;
 
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::info};
 use components::{Position, Symbol};
 
 pub fn plugin(app: &mut App) {
     app.add_systems(Startup, |mut commands: Commands| {
+        debug!("debug");
         commands.spawn((Symbol('@'), Position { x: 20, y: 7 }));
     });
 }
